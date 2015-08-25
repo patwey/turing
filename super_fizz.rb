8c563f598d042f64836af9e94dbdde56c621b6ec
Pat Wey <patrickwey@gmail.com>
2015-08-24 23:17:50 -0600

    added superfizz hw

---
+++ super_fizz.rb
@@ -0,0 +1,28 @@
+# clear
+(1..1000).each do |n|
+  if n % 3 == 0 && n % 5 == 0 && n % 7 == 0
+    puts 'SuperFizzBuzz'
+  elsif n % 3 == 0 && n % 7 == 0
+    puts 'SuperFizz'
+  elsif n % 5 == 0 && n % 7 == 0
+    puts 'SuperBuzz'
+  elsif n % 3 == 0
+    puts 'Fizz'
+  elsif n % 5 == 0
+    puts 'Buzz'
+  elsif n % 7 == 0
+    puts 'Super'
+  else
+    puts n
+  end
+end
+
+# concise
+(1..1000).each do |n|
+  output = ''
+  output += 'Super' if n % 7 == 0
+  output += 'Fizz' if n % 3 == 0
+  output += 'Buzz' if n % 5 == 0
+  output += n.to_s if output == ''
+  puts output
+end
